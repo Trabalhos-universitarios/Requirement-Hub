f9ba254a6ed0f144dfdb53dee66caebb3f028c32
eliasfernandescout <eliasfernandescout@gmail.com>
2024-02-23 11:12:15 -0300

    first commit

---
+++ src/main/java/com/br/requirementhub/RequirementhubApplication.java
@@ -0,0 +1,13 @@
+package com.br.requirementhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequirementhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequirementhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/requirementhub/RequirementhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.requirementhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequirementhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
