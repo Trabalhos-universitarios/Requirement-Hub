1f82f9340ea042e1c7133b33462618fc5336540f
JohnnyCarvalho <johnnycarvalhodev@gmail.com>
2024-08-13 20:44:32 -0300

    Removendo arquivo jackson.

--- src/main/java/com/br/requirementhub/dtos/requirement/RequirementResponseDTO.java
+++ src/main/java/com/br/requirementhub/dtos/requirement/RequirementResponseDTO.java
@@ -1,9 +1,5 @@
 package com.br.requirementhub.dtos.requirement;
 
-import com.br.requirementhub.utils.DateUtils;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.Set;
 import lombok.Data;
 
@@ -21,11 +17,6 @@ public class RequirementResponseDTO {
     private String status;
     private Integer effort;
     private Long projectId;
-//    @JsonFormat(
-//            shape = JsonFormat.Shape.STRING,
-//            pattern = "yyyy-MM-dd'T'HH:mm:ss"
-//    )
-//    private LocalDateTime dateCreated;
     private String dateCreated;
     private Set<Long> artifactIds;
     private Set<Long> responsibleIds;
