0a2bc85829fae90e2d14b757e94f5bdc0c39608d
eliasfernandescout <eliasfernandescout@gmail.com>
2024-10-15 12:19:22 -0300

    alterando rota no SecurityFilter

--- src/main/java/com/br/requirementhub/config/SecurityFilter.java
+++ src/main/java/com/br/requirementhub/config/SecurityFilter.java
@@ -37,7 +37,7 @@ public class SecurityFilter {
                     authConfig.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll();
 
 
-                    authConfig.requestMatchers(HttpMethod.POST, "/auth/login").permitAll();
+                    authConfig.requestMatchers(HttpMethod.POST, "/auth/authenticate").permitAll();
                     authConfig.requestMatchers(HttpMethod.POST, "/auth/register").hasRole(ADMIN.name());
 
                     authConfig.requestMatchers(HttpMethod.DELETE, "/auth/**").hasRole(ADMIN.name());
